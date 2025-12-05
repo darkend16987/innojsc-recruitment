9417b902780441eec853dbcb13d4b3c655a09370
darkend16987 <hoangnam.mng@gmail.com>
2025-12-05 08:47:19 +0700

    change hero image about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -176,7 +176,7 @@ export default function AboutPage() {
         {/* Background Image */}
         <div className="absolute inset-0 z-0">
           <Image
-            src="/images/recruitment/hero/about-hero.webp"
+            src="/images/recruitment/hero/about-hero1.webp"
             alt="Về INNO"
             fill
             className="object-cover"
